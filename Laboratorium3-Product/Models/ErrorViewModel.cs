5540660cfe8d0d53fd7d12d43c7d6270e7f623b7
Justyna Malinowska <justynamalinowska758@gmail.com>
2023-10-24 13:37:42 +0200

    Create Product

---
+++ Laboratorium3-Product/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Laboratorium3_Product.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
