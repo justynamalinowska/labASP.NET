dee464c9a3801c4cf253caf1d2133fc3d1cc2927
Justyna Malinowska <justynamalinowska758@gmail.com>
2023-10-26 17:09:24 +0200

    Final commit

--- Laboratorium3-Product/Models/MemoryProductService.cs
+++ Laboratorium3-Product/Models/MemoryProductService.cs
@@ -7,18 +7,11 @@ namespace Laboratorium3.Models
     public class MemoryProductService : IProductService
     {
         private Dictionary<int, Product> _items = new Dictionary<int, Product>();
-        //private IDateTimeProvider _timeProvider;
-
-        //public MemoryContactService(IDateTimeProvider timeProvider)
-        //{
-        //    _timeProvider = timeProvider;
-        //}
 
         public int Add(Product item)
         {
             int id = _items.Keys.Count != 0 ? _items.Keys.Max() : 0;
             item.Id = id + 1;
-            //item.Created = _timeProvider.GetDateTime();
             _items.Add(item.Id, item);
             
             return item.Id;
