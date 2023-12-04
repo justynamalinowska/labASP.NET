5905e1c0e469d462384db79632f95621afb25832
Justyna Malinowska <justynamalinowska758@gmail.com>
2023-12-04 18:51:16 +0100

    Change `Create` for a product

--- Laboratorium3-Product/Controllers/ProductController.cs
+++ Laboratorium3-Product/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Laboratorium3_Product.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -29,7 +30,19 @@ namespace Laboratorium3_Product.Controllers
         [HttpGet]
         public IActionResult Create()
         {
-            return View();
+            return View(new Product() { ProducentList = CreateSelectItem() });
+        }
+        
+        private List<SelectListItem> CreateSelectItem()
+        {
+            var items = _productService.FindAllProducents()
+                .Select(e => new SelectListItem()
+                {
+                    Text = e.Name,
+                    Value = e.Id.ToString()
+                }).ToList();
+            items.Add(new SelectListItem() { Text = "Unknown", Value = "" });
+            return items;
         }
 
         [HttpPost]
